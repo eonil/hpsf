92d052f0d2d195e1c68adf688fbd229637eef7e7
eonil <pub.dev@eonil.com>
2023-11-18 00:19:09 +0800

    Initial import.

---
+++ HPSSContent/Sources/HPApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPSSApp.swift
+//  HPSS
+//
+//  Created by Hoon H. on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct HPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HPContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+HPSS
+====
+
+HPSS stands for "High Performance SwiftUI Samples".
+This is essentially a sample app code which shows how to make high performance apps UI using SwiftUI.
+
